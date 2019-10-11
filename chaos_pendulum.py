dfcf171580f872b97b4fb17ddbc717766bff5235
giaco <muzlu@hotmail.com>
2019-10-12 00:41:06 +0200

    fractal phasespace

--- chaos_pendulum.py
+++ chaos_pendulum.py
@@ -58,10 +58,10 @@ def calculate_trajectory(n_iter,dt,theta1_init,theta2_init,theta1_d_init,theta2_
 	phase_traject=np.zeros((n_iter,4))#phase-space trajectory
 	phase_traject[0,:]=np.array([theta1_init,theta2_init,theta1_d_init,theta2_d_init])
 	for i in range(n_iter-1):
-		if i%100000==0:
+		if (i+1)%100000==0:
 			print('progress: '+str(i)+'/'+str(n_iter-1))
 		if add_energy is not None:
-			phase_traject[i,2]+=np.sign(phase_traject[i,2])*1e-5
+			phase_traject[i,2]+=np.sign(phase_traject[i,2])*add_energy
 		#---explicite Euler ----
 		# theta1,theta2,theta1_d,theta2_d=explicite_euler(dt,phase_traject[i,0],phase_traject[i,1],phase_traject[i,2],phase_traject[i,3],m1,m2,l1,l2,g)
 		# phase_traject[i+1,:]=np.array([theta1,theta2,theta1_d,theta2_d])
@@ -132,7 +132,7 @@ def distance_in_phase_space(phase_point):
 		phase_point[1]=2*np.pi-phase_point[1]
 	return np.linalg.norm(phase_point)
 
-def render_phase_traject(phase_traject,img_res=1,m1=1,m2=1,l1=1,l2=0.5,g=10,save_path='trash_figures/',take_frame_every=1,second_phase_traject=None,draw_phase=True,draw_marker=True,max_points=500,frames_per_second=20):
+def render_phase_traject(phase_traject,img_res=1,m1=1,m2=1,l1=1,l2=0.5,g=10,save_path='trash_figures/',take_frame_every=1,second_phase_traject=None,draw_phase=True,draw_marker=True,max_points=500,frames_per_second=20,show_energy=False):
 	frames=[]
 	e_pot=[]#the potential energy of each of the masses: e_pot=-m*g*y
 	e_kin=[]#the kinetic energy of each of the masses: e_kin=m*l**2*theta_d**2/2
@@ -209,7 +209,7 @@ def render_phase_traject(phase_traject,img_res=1,m1=1,m2=1,l1=1,l2=0.5,g=10,save
 						phase_xx=w_34+x0*((prev_phase[-idx+1][0]+np.pi)%(2*np.pi)-np.pi)/(2*np.pi)
 						phase_yy=y0+y0*prev_phase[-idx+1][1]/max_theta2_d
 						draw.line([(phase_x,phase_y),(phase_xx,phase_yy)],fill=(255,intensity,intensity),width=2)
-						if idx==2:
+						if idx==2:							
 							draw.ellipse([(phase_xx-d1,phase_yy-d1),(phase_xx+d1,phase_yy+d1)], fill=(255,0,0), outline=None)
 					if np.abs((prev_phase[-idx][2]+np.pi)%(2*np.pi)-(prev_phase[-idx+1][2]+np.pi)%(2*np.pi))<np.pi:
 						phase_x=w_34+x0*((prev_phase[-idx][2]+np.pi)%(2*np.pi)-np.pi)/(2*np.pi)
@@ -248,25 +248,28 @@ def render_phase_traject(phase_traject,img_res=1,m1=1,m2=1,l1=1,l2=0.5,g=10,save
 					draw.line([(bias+dx*idx,int(bias_y+h/4*(2-prev_distances[-idx-1]))),(bias+dx*(idx+1),int(bias_y+h/4*(2-prev_distances[-idx-2])))],fill=(intensity,intensity,255),width=2)
 					idx+=1
 
-			draw.line([(x0,y0),(x1,y1)],fill=(0,0,0),width=3)		
-			draw.line([(x1,y1),(x2,y2)],fill=(0,0,0),width=3)
+			draw.line([(x0,y0),(x1,y1)],fill=(0,0,0),width=2)		
+			draw.line([(x1,y1),(x2,y2)],fill=(0,0,0),width=2)
 			if second_phase_traject is None:
 				draw.ellipse([(x1-d1,y1-d1),(x1+d1,y1+d1)], fill=(255,0,0), outline=None)
 				draw.ellipse([(x2-d2,y2-d2),(x2+d2,y2+d2)], fill=(0,0,255), outline=None)
 			else:
 				draw.ellipse([(x1-d1,y1-d1),(x1+d1,y1+d1)], fill=(0,0,0), outline=None)
 				draw.ellipse([(x2-d2,y2-d2),(x2+d2,y2+d2)], fill=(0,0,0), outline=None)
-			frames.append(img)
 			#----calculate the energies----
 			e_pot_i,e_kin_i=get_energy(theta1,theta2,theta1_d,theta2_d,m1,m2,l1,l2,g)
+			if show_energy:
+				font = ImageFont.truetype("arial.ttf", int(h/20))
+				draw.text((int(h/20),int(h/10)), 'energy: '+str(np.sum(e_pot_i)+np.sum(e_kin_i))[:4], font=font, fill=(0,0,0))
+
 			e_pot.append(e_pot_i)
 			e_kin.append(e_kin_i)
-
-	frames[0].save(save_path+'double_pendulum.gif',
-	               save_all=True,
-	               append_images=frames[1:],
-	               duration=40,
-	               loop=0)
+			frames.append(img)
+	#frames[0].save(save_path+'double_pendulum.gif',
+	               #save_all=True,
+	               #append_images=frames[1:],
+	               #duration=40,
+	               #loop=0)
 	cv2_list=pil_list_to_cv2(frames)
 	generate_video(cv2_list,path=save_path+'double_pendulum.avi',fps=frames_per_second)
 
@@ -275,21 +278,129 @@ def render_phase_traject(phase_traject,img_res=1,m1=1,m2=1,l1=1,l2=0.5,g=10,save
 	print('final energy: '+str(np.sum(e_pot[-1,:])+np.sum(e_kin[-1,:])))
 	return e_pot,e_kin
 
+def represent_set_of_trajectories(trajectory_list,img_res=1,m1=1,m2=1,l1=1,l2=0.5,g=10,save_path='trash_figures/',take_frame_every=1,frames_per_second=20):
+	frames=[]
+	h=int(img_res*200)
+	w=2*h
+	w_34=int(3*w/4)
+	x0=int(h/2)
+	y0=int(h/2)
+	h_red=int(0.4*h)
+	d=int(0.02*h)
+	# max_theta2_d=1.2*np.max(np.abs(phase_traject[:,3]))
+	max_theta2_d=10
+	for i in range(1,trajectory_list[0].shape[0]):
+		if i%10000==0:
+			print('rendering iteration: '+str(i)+'/'+str(trajectory_list[0].shape[0]))   
+		if i%take_frame_every==0:
+			#---draw the image ----
+			img = Image.new("RGB", (w, h), "white")
+			draw = ImageDraw.Draw(img)
+			if i<400:
+				#--theta1 submanifold
+				draw_arrow(draw,np.asarray([int(1*h/8),int(h/2)]),np.asarray([int(7*h/8),int(h/2)]),flank_length=int(h/50))#horizontal
+				draw_arrow(draw,np.asarray([int(h/2),int(7*h/8)]),np.asarray([int(h/2),int(1*h/8)]),flank_length=int(h/50))#vertical
+				font = ImageFont.truetype("arial.ttf", int(h/24))
+				draw.text((h/2-3*h/14,int(1*h/16)), 'inner angular velocity', font=font, fill=(0,0,0))
+				draw.text((int(6*h/8),int(h/2+h/30)), 'inner angle', font=font, fill=(0,0,0))
+				#--theta2 submanifold
+				draw_arrow(draw,np.asarray([int(9*h/8),int(h/2)]),np.asarray([int(15*h/8),int(h/2)]),flank_length=int(h/50))#horizontal
+				draw_arrow(draw,np.asarray([w_34,int(7*h/8)]),np.asarray([w_34,int(1*h/8)]),flank_length=int(h/50))#vertical
+				font = ImageFont.truetype("arial.ttf", int(h/24))
+				draw.text((w_34-3*h/14,int(1*h/16)), 'outer angular velocity', font=font, fill=(0,0,0))
+				draw.text((int(14*h/8),int(h/2+h/30)), 'outer angle', font=font, fill=(0,0,0))
+			for phase_traject in trajectory_list:
+				phase_x1=x0+x0*((phase_traject[i,0]+np.pi)%(2*np.pi)-np.pi)/(2*np.pi)
+				phase_y1=y0+y0*phase_traject[i,2]/max_theta2_d
+				if np.abs((phase_traject[i,0]+np.pi)%(2*np.pi)-(phase_traject[i-1,0]+np.pi)%(2*np.pi))<np.pi:
+					phase_xx1=x0+x0*((phase_traject[i-1,0]+np.pi)%(2*np.pi)-np.pi)/(2*np.pi)
+					phase_yy1=y0+y0*phase_traject[i-1,2]/max_theta2_d
+					draw.line([(phase_x1,phase_y1),(phase_xx1,phase_yy1)],fill=(255,150,150),width=1)
+				phase_x2=w_34+x0*((phase_traject[i,1]+np.pi)%(2*np.pi)-np.pi)/(2*np.pi)
+				phase_y2=y0+y0*phase_traject[i,2]/max_theta2_d
+				if np.abs((phase_traject[i,1]+np.pi)%(2*np.pi)-(phase_traject[i-1,1]+np.pi)%(2*np.pi))<np.pi:
+					phase_xx2=w_34+x0*((phase_traject[i-1,1]+np.pi)%(2*np.pi)-np.pi)/(2*np.pi)
+					phase_yy2=y0+y0*phase_traject[i-1,2]/max_theta2_d
+					draw.line([(phase_x2,phase_y2),(phase_xx2,phase_yy2)],fill=(150,150,255),width=1)
+				draw.point((phase_x1,phase_y1), fill=(255,0,0))
+				draw.point((phase_x2,phase_y2), fill=(0,0,255))
+			frames.append(img)
+
+	frames[0].save(save_path+'point_cloud.gif',
+	               save_all=True,
+	               append_images=frames[1:],
+	               duration=40,
+	               loop=0)
+	cv2_list=pil_list_to_cv2(frames)
+	generate_video(cv2_list,path=save_path+'point_cloud.avi',fps=frames_per_second)
+
+
+def draw_fractal(dt=0.005,theta1_lower=-3,theta1_higher=3,theta2_lower=-3,theta2_higher=3,img_res=1,m1=1,m2=1,l1=1,l2=0.5,g=10,save_path='trash_figures/'):
+	H=int(200*img_res)
+	W=H
+	delta1=(theta1_higher-theta1_lower)/W
+	delta2=(theta2_higher-theta2_lower)/H
+	max_iter=10000
+	img = Image.new("RGB", (W, H), "white")
+	px=img.load()
+	p=0
+	for w in range(W):
+		for h in range(H):
+			print('calculate pixel '+str(p)+'/'+str(W*H))
+			p+=1
+			theta1=theta1_lower+w*delta1
+			theta2=theta2_lower+h*delta2
+			theta1_d=0
+			theta2_d=0
+			flip=False
+			n_iter=0
+			while not flip and n_iter<max_iter:
+				n_iter+=1
+				#---explicite midpoint method ----
+				theta11,theta22,theta11_d,theta22_d=explicite_euler(dt/2,theta1,theta2,theta1_d,theta2_d,m1,m2,l1,l2,g)
+				theta11_dd,theta22_dd=get_theta_dd(theta11,theta22,theta11_d,theta22_d,m1,m2,l1,l2,g)
+				theta1_dd,theta2_dd=get_theta_dd(theta11,theta22,theta11_d,theta22_d,m1,m2,l1,l2,g)
+				theta1_d_new=theta1_d+dt*theta1_dd
+				theta2_d_new=theta2_d+dt*theta2_dd
+				theta1+=dt/2*(theta1_d+theta1_d_new)
+				theta2+=dt/2*(theta2_d+theta2_d_new)
+				theta1_d=theta1_d_new
+				theta2_d=theta2_d_new				
+				if not -np.pi<theta2<np.pi:
+					flip=True
+			intensity=int(255*n_iter/max_iter)
+			px[w,h]=(intensity,intensity,intensity)
+	img.show()
 
 theta1_init=4*np.pi/8
 theta2_init=1*np.pi/8
 theta1_d_init=0
 theta2_d_init=0
 dt=0.005
-frames_per_second=25
+frames_per_second=50
 take_frame_every=int(1/(dt*frames_per_second))
-n_iter=6000
-m2=0.8
-print('calculate trajectory...')
-phase_traject=calculate_trajectory(n_iter,dt,theta1_init,theta2_init,theta1_d_init,theta2_d_init,m2=m2)
-# phase_traject_2=calculate_trajectory(n_iter,dt,theta1_init+1e-2,theta2_init+1e-2,theta1_d_init,theta2_d_init,m2=m2)
-print('render phase trajectory...')
-e_pot,e_kin=render_phase_traject(phase_traject,img_res=2,take_frame_every=take_frame_every,m2=m2,second_phase_traject=None,draw_phase=True,draw_marker=True,max_points=100,frames_per_second=frames_per_second)
+n_iter=4000
+m2=0.7
+
+
+#---draw star_dance---
+# phase_traject_list=[]
+# for i in range(1000):
+# 	if i%20==0:
+# 		print('trajectory: '+str(i))
+# 	r=np.random.rand(4)*2e-2
+# 	phase_traject=calculate_trajectory(n_iter,dt,theta1_init+r[0],theta2_init+r[1],theta1_d_init+r[2],theta2_d_init+r[3],m2=m2,l1=1,l2=0.5)
+# 	phase_traject_list.append(phase_traject)
+# represent_set_of_trajectories(phase_traject_list,img_res=2,m2=m2,l1=1,l2=0.5,save_path='trash_figures/',take_frame_every=take_frame_every,frames_per_second=int(frames_per_second/2))
+
+
+#---draw fractal
+draw_fractal(dt=0.005,theta1_lower=-3,theta1_higher=3,theta2_lower=-3,theta2_higher=3,img_res=0.1,m1=1,m2=1,l1=1,l2=0.5,g=10,save_path='trash_figures/')
+
+
+# phase_traject=calculate_trajectory(n_iter,dt,theta1_init,theta2_init,theta1_d_init,theta2_d_init,m2=m2,l1=1,l2=0.5)
+# phase_traject_2=calculate_trajectory(n_iter,dt,theta1_init+1e-2,theta2_init+1e-2,theta1_d_init,theta2_d_init,m2=m2,l1=1,l2=0.5,add_energy=None)
+# e_pot,e_kin=render_phase_traject(phase_traject,img_res=2,take_frame_every=take_frame_every,m2=m2,l1=1,l2=0.5,second_phase_traject=None,draw_phase=False,draw_marker=True,max_points=500,frames_per_second=frames_per_second,show_energy=False)
 # plt.plot(np.sum(e_pot,axis=1)+np.sum(e_kin,axis=1))
 # plt.ylabel('energy')
 # plt.show()
